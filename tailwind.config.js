5f07253b632914c679b39908550927d4f1007f2a
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2024-11-09 12:29:00 +0700

    push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#1a202c',
+      },
+    },
+  },
+  plugins: [],
+};
