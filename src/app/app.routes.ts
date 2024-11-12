3647d6683a65485a4d9579b87d5aa073b7366db1
lenguyenthehien2205 <lenguyenthehien2205@gmail.com>
2024-11-12 22:37:55 +0700

    push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
     },
     {
         path: '',
-        redirectTo: 'login',
+        redirectTo: 'home',
         pathMatch: 'full'
     },
     {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent{
+  loginService = inject(LoginService);
+  token = this.loginService.getToken();
 }
